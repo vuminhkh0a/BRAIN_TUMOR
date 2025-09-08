dbdc54c4efd3a4cb331b096641c84c1a88421b18
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-08 11:26:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Brain Tumor classification')
 
-input_image = st.file_uploader(type=["jpg", "jpeg", "png"])
+input_image = st.file_uploader('Upload image', type=["jpg", "jpeg", "png"])
 
 if input_image:
   st.write('Received')

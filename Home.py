f144dd12b75aa42ad1a2071af9b9ac607ceff0fd
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-08 22:01:32 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,31 +15,13 @@ from sklearn.metrics import multilabel_confusion_matrix, ConfusionMatrixDisplay
 
 import model
 from model import test_transform, model
-# import page1, page2
-# from page1 import show_page1
-# from page2 import show_page2
-
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 st.title('Brain MRI Diagnosis App')
+st.write('The model will take brain scan MRI image (.jpg, .jpeg, .png) as input')
+st.write('The output of the model will be one of four classes: "Glioma", "Meningioma", "No Tumor", or "Pituitary"')
 
-# if st.button("Home"):
-#     st.switch_page("streamlit_app.py")
-# if st.button("How to use"):
-#     st.switch_page("pages/page1.py")
-# if st.button("About me"):
-#     st.switch_page("pages/page2.py")
-
-# selected = option_menu(
-#             menu_title=None,  # required
-#             options=["Home", "How to use", "About me"],  # required
-#             default_index=0,  # optional
-#             orientation="horizontal",
-# )
-
-# if selected == 'Home':
 input_image = st.file_uploader('Please upload your brain MRI image', type=["jpg", "jpeg", "png"])
 
 if input_image is not None:
@@ -66,10 +48,7 @@ if input_image is not None:
     plt.xlabel('Confidences')
     plt.ylabel('Classes')
     st.pyplot(fig)
-# if selected == 'How to use':
-#             show_page1()
-# if selected == 'About me':
-#             show_page2()
+
 
 
 

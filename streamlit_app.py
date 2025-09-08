1c6c42c68b09d11278b90c1ddb1ca1c2d8d25635
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-08 18:00:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ import torch.nn as nn
 from torchvision.transforms import v2
 from torchvision.models import resnet50
 from sklearn.metrics import multilabel_confusion_matrix, ConfusionMatrixDisplay
+import model
+from model import test_transform, model
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -28,41 +30,41 @@ else:
   st.write('No image is chosen!')
 
 
-test_transform = v2.Compose([
-    v2.PILToTensor(),
-    v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    v2.Resize((256, 256))
-])
+# test_transform = v2.Compose([
+#     v2.PILToTensor(),
+#     v2.ToDtype(torch.float32, scale=True),
+#     v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#     v2.Resize((256, 256))
+# ])
 
-class Classifier(nn.Module):
-    def __init__(self, n_output_class):
-        super().__init__()
-        self.dropout = nn.Dropout()
-        self.linear = nn.Linear(2048, n_output_class)
+# class Classifier(nn.Module):
+#     def __init__(self, n_output_class):
+#         super().__init__()
+#         self.dropout = nn.Dropout()
+#         self.linear = nn.Linear(2048, n_output_class)
     
-    def forward(self, x):
-        x = x.view(x.size(0), -1)
-        x = self.dropout(x)
-        x = self.linear(x)
-        return x
+#     def forward(self, x):
+#         x = x.view(x.size(0), -1)
+#         x = self.dropout(x)
+#         x = self.linear(x)
+#         return x
 
-class Backbone(nn.Module):
-    def __init__(self):
-        super().__init__()
-        base_model = resnet50(pretrained=False)
-        encoder_layers = list(base_model.children())
-        self.backbone = nn.Sequential(*encoder_layers[:9])
+# class Backbone(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         base_model = resnet50(pretrained=False)
+#         encoder_layers = list(base_model.children())
+#         self.backbone = nn.Sequential(*encoder_layers[:9])
     
-    def forward(self, x):
-        x = self.backbone(x)
-        return x
+#     def forward(self, x):
+#         x = self.backbone(x)
+#         return x
       
-backbone = Backbone()
-classifier = Classifier(4) 
-model = nn.Sequential(backbone, classifier)
-model.load_state_dict(torch.load('best_model_Sequential.pth', weights_only=True, map_location=torch.device('cpu')))
-model = model.to(device)
+# backbone = Backbone()
+# classifier = Classifier(4) 
+# model = nn.Sequential(backbone, classifier)
+# model.load_state_dict(torch.load('best_model_Sequential.pth', weights_only=True, map_location=torch.device('cpu')))
+# model = model.to(device)
 
 if input_image is not None:
   model.eval()

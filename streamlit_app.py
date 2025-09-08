c7337aacce1927c3a1a074ca894d7060eaa47fc4
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-08 11:52:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ if input_image:
   st.write('Received')
   input_image = np.asarray(bytearray(input_image.read()))
 
-plt.imshow(input_image)
+plt.imshow(input_image.permute(1, 2, 0))
 
 test_transform = v2.Compose([
     v2.PILToTensor(),

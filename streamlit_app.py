44f7affa3ed1a3ef8594dc58b32253b6da700a21
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-08 18:04:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if input_image is not None:
     encode_label = {0:'glioma', 1:'meningioma', 2:'notumor', 3:'pituitary'}
     st.write('Predicted class:', encode_label[np.argmax(prediction)])
     fig = plt.figure(figsize=(10, 10))
-    sns.barplot(x=prediction, y=['glioma', 'meningioma', 'notumor', 'pituitary'])
+    sns.barplot(x=prediction, y=['Glioma', 'Meningioma', 'No tumor', 'Pituitary'], hue='Subcategory')
     plt.xlabel('Confidences')
     plt.ylabel('Classes')
     st.pyplot(fig)

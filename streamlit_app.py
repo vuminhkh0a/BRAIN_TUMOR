aeb3a08d05428e079b28c5f6aad1dbb503bac177
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-08 14:08:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,8 +67,9 @@ if input_image is not None:
   with torch.no_grad():
     input_image = test_transform(input_image).to(device)
     prediction = model(input_image.unsqueeze(0))
-    _, scale = torch.max(prediction, 1)
-    prediction /= scale
+    sm = m = nn.Softmax(dim=1)
+    
+    prediction = sm(prediction)
     st.write(prediction)
 
 

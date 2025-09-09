3bf344c6626d80f94a0732058ead0c1ec8912ca4
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-09 16:47:18 +0700

    Update 3_About me.py

--- pages/3_About me.py
+++ pages/3_About me.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.title('About me')
-st.write('My name is Khoa, I am a sophomore at HUST')
+st.write('My name is Khoa, I am a sophomore at Hanoi University of Science and Technology. I am interested in Computer Vision research')
+st.link_button(label='Github', url='https://github.com/vuminhkh0a/vuminhkh0a')
+st.link_button(label='Linkedin', url='https://www.linkedin.com/in/vmkhoa28/')
+st.link_button(label='Gmail', url='vuminhkhoa2802@gmail.com')
+

99ab11e2c63d41881093ddc596fd3862fbeef29b
Vũ Minh Khoa <vuminhkhoa2802@gmail.com>
2025-09-09 17:50:52 +0700

    Update 3_About me.py

--- pages/3_About me.py
+++ pages/3_About me.py
@@ -9,17 +9,42 @@ def show_contact_form():
 
 
 # --- HERO SECTION ---
+# col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+# with col1:
+#     st.image("assets/profile_image.png", width=230)
+
+# with col2:
+#     st.title("Vu Minh Khoa", anchor=False)
+#     st.write("Sophomore at Hanoi University of Science and Technology, interested in Computer Vision for medical and healthcare research")
+#     st.link_button(label='Github', url='https://github.com/vuminhkh0a')
+#     st.link_button(label='Linkedin', url='https://www.linkedin.com/in/vmkhoa28/')
+#     if st.button("✉️ Contact Me"):
+#         show_contact_form()
+
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+
 with col1:
     st.image("assets/profile_image.png", width=230)
 
 with col2:
     st.title("Vu Minh Khoa", anchor=False)
-    st.write("Sophomore at Hanoi University of Science and Technology, interested in Computer Vision for medical and healthcare research")
-    st.link_button(label='Github', url='https://github.com/vuminhkh0a')
-    st.link_button(label='Linkedin', url='https://www.linkedin.com/in/vmkhoa28/')
-    if st.button("✉️ Contact Me"):
-        show_contact_form()
+    st.write(
+        "Sophomore at Hanoi University of Science and Technology, "
+        "interested in Computer Vision for medical and healthcare research"
+    )
+
+    btn_col1, btn_col2, btn_col3 = st.columns(3, gap="small")
+
+    with btn_col1:
+        st.link_button(label="Github", url="https://github.com/vuminhkh0a")
+
+    with btn_col2:
+        st.link_button(label="Linkedin", url="https://www.linkedin.com/in/vmkhoa28/")
+
+    with btn_col3:
+        if st.button("✉️ Contact Me"):
+            show_contact_form()
+
 
 
 # --- EXPERIENCE & QUALIFICATIONS ---
